1e7d02be7913863306ee2d0a0aa45326ec54ec6b
Web Source (SRC) <61905565+web-sys1@users.noreply.github.com>
2020-08-25 11:50:18 +0300

    r

--- test/test_main.py
+++ test/test_main.py
@@ -60,5 +60,5 @@ subscriptions = [
   'http://doingthatwrong.com/?format=rss']
   
 with open(f'feeds.txt', 'w', encoding='utf8') as f:
-    NFSconfig.init()
+    NFSinit()
     f.write(",".join(subscriptions).replace(',', '\n'))
