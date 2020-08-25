a6fb7d980e191bc48e30408bd3de493dab5a4450
Web Source (SRC) <61905565+web-sys1@users.noreply.github.com>
2020-08-25 13:23:20 +0300

    Update testcase.py

--- test/testcase.py
+++ test/testcase.py
@@ -1,7 +1,16 @@
-import subprocess
+from NFSyndication import main as NFS_init
+import os
+
+subscriptions = [
+  'http://feedpress.me/512pixels',
+  'http://www.leancrew.com/all-this/feed/',
+  'http://ihnatko.com/feed/',
+  'http://blog.ashleynh.me/feed']
+  
+with open(f'feeds.txt', 'w', encoding='utf8') as f:
+    f.write(",".join(subscriptions).replace(',', '\n'))
 
 def entry_point():
-  try:
-   subprocess.run(["nfsyndication-src"])
-  except Exception:
-   print('ERROR')
+  return NFS_init()
+
+print(entry_point())
