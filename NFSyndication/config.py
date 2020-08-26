e5a11152fc39baf3d5a78c8e60add32bf47d76da
Web Source (SRC) <61905565+web-sys1@users.noreply.github.com>
2020-08-26 21:34:12 +0300

    Update config.py

--- NFSyndication/config.py
+++ NFSyndication/config.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python 
+import sys
+from colored import  fg, bg, attr
 
 def init():
   try:
    from NFSyndication import nfs_main, styles
   except Exception as e:
-   print(f'Fatal: {e}')
+   print(f"%s%sFatal: {e} %s"%(fg(1),bg(0),attr('reset')))
+   sys.exit(1)
   
   #return os.popen(f'{dir_path}/nfs_main.py').read()
   #run = lambda filename : exec(open(filename).read(), globals())
